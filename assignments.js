a5452d7e4d83df167aef8060d79a48192c1e0aaf
Jacob Shi <68439204+EmpardFlop@users.noreply.github.com>
2021-05-15 19:28:06 -0400

    first commit
    added stuff

---
+++ assignments.js
@@ -0,0 +1,17 @@
+const country = 'Canada';
+const continent = 'North America';
+let population = 37590000;
+const isisland = false;
+let language = 'English';
+
+let description = country + ' is in ' + continent + ', and its ' + population + ' speaks ' + language;
+
+console.log(country, continent, population, isisland, language);
+
+console.log(population/2);
+
+console.log(population++);
+
+console.log(population > 6000000, population > 33000000);
+
+console.log(description);
---
+++ script.js
@@ -0,0 +1,24 @@
+// Variables and values
+
+let isJacobSmart = true;
+
+isJacobSmart = 69;
+
+const classType = "Pepega";
+const pepegaClap = 'Jacob';
+const spam = "777777777";
+const templateLiteral = `My name is ${pepegaClap}, 
+and I swear I'm not a ${classType}. 
+Diarisis, Diarisis ${spam}`;
+
+/*
+In JavaScript, it is possible to change values for variables.
+You cannot use another let statement, since it has already been declared
+*/
+
+console.log("The answer to is jacob smart is: " + isJacobSmart);
+console.log(typeof typeof typeof typeof typeof true);
+console.log(typeof 23);
+console.log(typeof null);
+console.log('pepega wr: ' + pepegaClap);
+console.log(templateLiteral)
